5a3eb34c504af69c5bf2f3287fdcd9107317acc5
Xinlin Zhou <XZhou@lcs.on.ca>
2024-01-12 08:52:36 -0500

    Added a stored property to hold the gradient colors (more organized).

--- Home/ContentView.swift
+++ Home/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Stored properties
+    let firstGradientColors = Gradient(colors: [Color.blue, Color.yellow])
+    
+    //MARK: Computed properties
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            // Background
+            LinearGradient(
+                gradient: firstGradientColors,
+                startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/,
+                endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/
+            )
+            .ignoresSafeArea()
+            
+            // Foreground
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
